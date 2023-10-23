5bbfddb7ee2fe374e662378c17395e5511158878
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-10-23 22:47:39 +0300

    TASK-1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,8 @@
-// @====================Task-1================================================
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const listUlGallery = document.querySelector(".gallery");
+
 const elementListGalleryLi = galleryItems
     .map(
         (element) => `<li class="gallery__item">
@@ -22,14 +22,27 @@ listUlGallery.insertAdjacentHTML(
     "afterbegin",
     elementListGalleryLi
 );
-// !=====================Task-2================================
+
 listUlGallery.addEventListener("click", selectImg);
+
 function selectImg(event) {
+    event.preventDefault();
     if (event.target.nodeName !== "IMG") {
         return;
     }
     const originalImg = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+        <img src="${originalImg}">
+    `);
+    instance.show();
+    document.addEventListener("keydown", onEscKeyPress);
+    function onEscKeyPress(event) {
+        if (event.code === "Escape") {
+            instance.close();
+            document.removeEventListener(
+                "keydown",
+                onEscKeyPress
+            );
+        }
+    }
 }
-// #=====================Task-3================================
-// $=====================Task-4================================
-// %=====================Task-5================================
