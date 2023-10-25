bd5280eeb1471448b7e2fd00ecf61d0b00235676
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-10-25 20:56:54 +0300

    task-3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listUlGallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const elementListGalleryLi = galleryItems
+    .map(
+        (element) => `<li class="gallery__item">
+    <a
+        class="gallery__link"
+        href="${element.original}"
+    >
+        <img
+            class="gallery__image"
+            src="${element.preview}"
+            alt="${element.description}"
+            title="${element.description}"
+        />
+    </a>
+</li>`
+    )
+    .join("");
+
+listUlGallery.insertAdjacentHTML(
+    "afterbegin",
+    elementListGalleryLi
+);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+});
